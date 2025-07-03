504ead28d2b4710cd7de790967dfa1ae09a57e91
DigitalLancer <ahmetsalimusul@yahoo.com>
2025-07-03 14:04:37 +0300

    Feature update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import ListElement from './components/ListElement'
 function App() {
   const [todos, setTodos] = useState([]);
   const [text, setText] = useState('');
+  const [highlights, setHighlights] = useState();
 
   useEffect(() => {
     fetch("http://localhost:3000/api/todos")
@@ -12,8 +13,29 @@ function App() {
         resp.json()
       ).then(data => {
         setTodos(data);
-      })
+      });
+    const savedHighlights = loadHighlights();
+    setHighlights(savedHighlights);
   }, [])
+
+  const loadHighlights = () => {
+    const stored = localStorage.getItem('highlights');
+    return stored ? JSON.parse(stored) : {};
+  }
+
+  const deleteHighlight = (id) => {
+    const colorMap = loadHighlights();
+    console.log("Element to be deleted from local storage with id", id);
+    delete colorMap[id];
+    setHighlights(colorMap);
+
+    if (Object.keys(colorMap).length === 0) {
+      localStorage.removeItem('highlights');
+    } else {
+      // Güncel halini kaydet
+      localStorage.setItem('highlights', JSON.stringify(colorMap));
+    }
+  }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -25,7 +47,7 @@ function App() {
       body: new URLSearchParams({ text }),
     });
     const data = await response.json();
-    console.log("Server response: \n",response,'\n',"Body: \n",data);
+    console.log("Server response: \n", response, '\n', "Body: \n", data);
     setText('');
     if (todos.length > 0) {
       setTodos(prev => [...prev, { id: todos[todos.length - 1].id + 1, text: text }])
@@ -35,7 +57,7 @@ function App() {
     }
   }
 
-  const handleDelete = async (e,id) => {
+  const handleDelete = async (e, id) => {
     e.preventDefault();
     const response = await fetch(`http://localhost:3000/api/todos/${id}`, {
       method: 'DELETE',
@@ -44,9 +66,10 @@ function App() {
       },
       body: new URLSearchParams({ text }),
     });
-    const data=await response.json();
+    const data = await response.json();
     setTodos(data);
-    console.log("Server response: \n",response,'\n',"Body: \n",data);
+    console.log("Server response: \n", response, '\n', "Body: \n", data);
+    deleteHighlight(id);
   }
 
   return (
@@ -54,11 +77,12 @@ function App() {
       <div className="page-container">
         <div className="content-container">
           <div className="todo-container">
+
             <h2 className="title">To Do List</h2>
             <ul id="todo-list">
               {todos.map((element) => (
                 <li key={element.id}>
-                  <ListElement data={element} onDelete={handleDelete} />
+                  <ListElement data={element} onDelete={handleDelete} highlightColor={highlights[element.id]} />
                 </li>
               ))}
             </ul>
--- client/src/components/ListElement.jsx
+++ client/src/components/ListElement.jsx
@@ -5,7 +5,7 @@ function ListElement(props) {
   const [text, setText] = useState(props.data.text);
   const [inputText, setInputText] = useState(props.data.text);
   const [editMode, setEditMode] = useState(false);
-  const [highlightColor, setHighlightColor] = useState('transparent');
+  const [highlightColor, setHighlightColor] = useState(props.highlightColor);
 
   const handleToggleEdit = () => {
     setEditMode(prevState => !prevState);
@@ -19,10 +19,23 @@ function ListElement(props) {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
       },
-      body: new URLSearchParams({ text:inputText }),
+      body: new URLSearchParams({ text: inputText }),
     });
-    
   }
+
+  const handleAddHighlight = (color) => {
+    setHighlightColor(color);
+    const stored = localStorage.getItem("highlights");
+    let colorMap = stored ? JSON.parse(stored) : {};
+    colorMap[props.data.id] = color;
+    localStorage.setItem('highlights', JSON.stringify(colorMap));
+    setEditMode(false);
+  }
+
+  /*const handleDeleteHighlight = (e) => {
+
+    props.onDelete(e,props.data.id);
+  }*/
 
   return (
     <>
@@ -30,28 +43,28 @@ function ListElement(props) {
         <div className="todo-content-container">
           <button className='check-btn' onClick={(e) => props.onDelete(e, props.data.id)}></button>
           <p style={{ 'backgroundColor': `${highlightColor}` }}>{text}</p>
-          <button className="edit-btn" onClick={handleToggleEdit}><i class="fa-solid fa-pencil"></i></button>
+          <button className="edit-btn" onClick={handleToggleEdit}><i className="fa-solid fa-pencil"></i></button>
         </div>
         <div className={`edit-form-container ${editMode ? 'active' : ''}`}>
           <div>
+            <h3>Edit task</h3>
             <form action="" style={{ 'display': 'flex' }} onSubmit={handleUpdateText}>
               <input type="text" id="text" value={inputText} onChange={(e) => setInputText(e.target.value)} />
               <button className='edit-submit-btn'><i class="fa-solid fa-check fa-xs"></i></button>
             </form>
           </div>
           <div className='color-pick'>
-            <h3>Highlight</h3>
+            <h3>Highlight Color</h3>
             <div className="colors-container">
-              <button className='highlight-color yellow' onClick={() => setHighlightColor('yellow')}></button>
-              <button className='highlight-color red' onClick={() => setHighlightColor('red')}></button>
-              <button className='highlight-color green' onClick={() => setHighlightColor('#44e200')}></button>
+              <button className='highlight-color yellow' onClick={() => handleAddHighlight('yellow')}></button>
+              <button className='highlight-color red' onClick={() => handleAddHighlight('red')}></button>
+              <button className='highlight-color green' onClick={() => handleAddHighlight('#44e200')}></button>
+              <button className='highlight-color transparent' onClick={() => handleAddHighlight('transparent')}></button>
             </div>
           </div>
         </div>
       </div>
-
     </>
-
   )
 }
 
